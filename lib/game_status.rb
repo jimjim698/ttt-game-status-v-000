48d5bf661a358a76d09835f8380435dc9d5e03c5
James Lotruglio <jimlotruglio@yahoo.com>
2017-11-25 23:57:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,9 +26,9 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if position_1 == "X" && position_2 == "X" && position_3 ==  "X"
        win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_1 ==  "O" && position_2 == "O" && position_3 == "O"
        win_combination
     else
       false
